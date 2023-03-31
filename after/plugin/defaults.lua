cf495c42326ec69f5447ceaeed426f340d646a7c
Benjamin F Jones <benjaminfjones@gmail.com>
2023-03-31 22:25:07 +0000

    Set some default options

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,5 @@
 vim.opt.relativenumber = true
+vim.opt.hlsearch = true
+vim.opt.conceallevel = 2
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
