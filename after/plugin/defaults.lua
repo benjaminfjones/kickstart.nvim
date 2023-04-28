bbde0f68140e74e1afc5bab6f55974852896171b
Benjamin F Jones <benjaminfjones@gmail.com>
2023-04-28 21:58:29 +0000

    Enable spell by default

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,3 +3,7 @@ vim.opt.hlsearch = true
 vim.opt.conceallevel = 2
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+-- spelling options: using Neovim + treesitter + spell integration!
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
